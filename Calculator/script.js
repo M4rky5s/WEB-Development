d5e0327d2561fde9b5f96a23b8495c34463dc117
Markas <markutis2255@gmail.com>
2025-10-19 21:04:19 +0300

    Padarytas Calculator dizainas

---
+++ Calculator/script.js
@@ -0,0 +1,20 @@
+let firstNumber;
+let secondNumber;
+let operator;
+
+function add(a, b){
+    return a + b;
+}
+function substract(a, b){
+    return a - b;
+}
+function multiply(a, b){
+    return a * b;
+}
+function divide(a, b){
+    return a / b;
+}
+
+function operate(numberOne, numberTwo, operator){
+
+}
